e62463730951756b1f155147424f54a3d1ce424c
jackie <jackiezh@wicrenet.com>
2020-04-17 17:57:12 +0800

    update-v4

--- src/main/java/com/jackie/controller/ReplaceController.java
+++ src/main/java/com/jackie/controller/ReplaceController.java
@@ -6,12 +6,10 @@ import com.jackie.com.jackie.entity.Result;
 import com.jackie.com.jackie.service.ConfigService;
 import com.jackie.com.jackie.service.ReplaceService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-@RestController(value = "/replaceConfig")
+@RestController
+@RequestMapping("replaceConfig")
 public class ReplaceController {
 
     @Autowired
